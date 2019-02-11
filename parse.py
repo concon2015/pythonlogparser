7ac9646c24ee32626cf89253c5c8f94dae578b63
concon2015 <connorblackard@gmail.com>
2019-02-11 13:57:36 -0600

    Cleaned up parsing via regex fuction.

--- parse.py
+++ parse.py
@@ -11,81 +11,67 @@ def parse():
     codefreq= {}
     monthlyUsageCounter = []
     monthlyUsageResults = []
-
+    prevmonth=""
+    #Regex function
     ##begin parsing
-    file = open("log","r")
+    file = open("log1","r")
     file_data =file.readlines()
     totalRequests = len(file_data)
     for i in file_data:
-        if len(i) > 60:
-            z = re.match('(\w+) - - \[(.*?)\] \"(.*?)\" (\d+) (\w+|-)',i)
-            print(z)
-            ##add to 300, 400 counters
-            if b[0]=='3':
-                status300count+=1
-            elif b[0]=='4':
-                status400count+=1
-            ##find the frequency of status codes
-            if(b in codefreq):
-                codefreq[b] += 1
-            else:
-                codefreq[b] = 1
-            ##find the frequency of accessed files
-
-
-            if a[0:3] == "GET":
-                if(a[4:-9] in accessedFiles):
-                    accessedFiles[a[4:-9]] += 1
+            try:
+                z = re.match('(\w+).*\[(.*?)\] \"\S+ (.*?)\" (\d+) (\w+|-)',i)
+                #300 and 400 counters
+                if z.group(4)[0]=="3":
+                    status300count+=1
+                elif z.group(4)[0]=="4":
+                    status400count+=1
+                #codefreq counters
+                if z.group(4) in codefreq:
+                    codefreq[z.group(4)]+=1
                 else:
-                    accessedFiles[a[4:-9]] = 1
-            elif a [0:3] == "HEA":
-                if(a[4:-9] in accessedFiles):
-                    accessedFiles[a[4:-9]] += 1
+                    codefreq[z.group(4)]=1
+                #accessedFiles counters
+                if z.group(3).split(' ')[0] in accessedFiles:
+                    accessedFiles[z.group(3).split(' ')[0]]+=1
                 else:
-                    accessedFiles[a[5:-9]] = 1
-        else:
-            malformedEntrys+=1
-
+                    accessedFiles[z.group(3).split(' ')[0]]=1
+            except AttributeError:
+                malformedEntrys+=1
     ##Create reports directory
     path = os.getcwd()+"/reports/monthlyLogs"
     try:
         os.makedirs(path)
     except OSError:
         print ("Directory %s currently exists" % path)
-    ##Create monthly file
+    #Create monthly file
     for i in file_data:
-        if len(i)>60:
-            m = path+"/"+(i[i.find('[')+8:i.find('[')+12]+"_"+i[i.find('[')+4:i.find('[')+7])
-            mreport = open(m, 'a+')
-            mreport.write(i)
-            mreport.close()
+            z = re.match('(\w+).*\[(.*?)\] \"\S+ (.*?)\" (\d+) (\w+|-)',i)
+            try:
+                m = path+"/"+(z.group(2)[3:6])+"_"+(z.group(2)[7:11])
+                prevmonth=(z.group(2)[3:6])+"_"+(z.group(2)[7:11])
+                mreport = open(m, 'a+')
+                mreport.write(i)
+                mreport.close()
+            except AttributeError:
+                m = path+"/"+(prevmonth)
+                mreport = open(m, 'a+')
+                mreport.write(i)
+                mreport.close()
     ##create reports
-    n = path+"/overallUsageReport"
-    nreport = open(n, 'w+')
-    nreport.write("This is report of detailed usage statistics\n")
-    nreport.write("===========================================\n")
-    nreport.write("        Monthly Usage Statistics           \n")
-    for i in file_data:
-        if len(i)>60:
-            g = i[i.find('[')+8:i.find('[')+12]+"_"+i[i.find('[')+4:i.find('[')+7]
-            if g in monthlyUsageCounter:
-                l=monthlyUsageCounter.index(g)
-                monthlyUsageResults[l]+=1
-            else:
-                monthlyUsageCounter.append(g)
-                monthlyUsageResults.append(1)
-    for i in monthlyUsageCounter:
-        j=monthlyUsageCounter.index(i)
-        nreport.write(monthlyUsageCounter[j] + '  -  ' + str(monthlyUsageResults[j]) + " requests\n")
-    nreport.write("===========================================\n")
-    nreport.write("                Statistics                 \n")
-    nreport.write("Total Request Count  -  " + str(totalRequests) + '\n')
-    nreport.write("3XX Status Codes  -  " +str(status300count) +' ('+ str(round(status300count/totalRequests*100,2)) +'%)\n')
-    nreport.write("4XX Status Codes  -  " +str(status400count) +' ('+ str(round(status400count/totalRequests*100,2)) +'%)\n')
-    nreport.write("Malformed Log Entries  -  " +str(malformedEntrys) +' ('+ str(round(malformedEntrys/totalRequests*100,2)) +'%)\n')
-    nreport.write("Most Requested File  -  " +str(max(accessedFiles, key=accessedFiles.get) +' ('+ str(accessedFiles.get(max(accessedFiles, key=accessedFiles.get), "none")) +' requests)\n'))
-    nreport.write("Least Requested File  -  " +str(min(accessedFiles, key=accessedFiles.get) +' ('+ str(accessedFiles.get(min(accessedFiles, key=accessedFiles.get), "none")) +' request)\n'))
-    # nreport.write("Status Codes:\n")
+    # n = path+"/overallUsageReport"
+    # nreport = open(n, 'w+')
+    # nreport.write("This is report of detailed usage statistics\n")
+    # nreport.write("===========================================\n")
+    # nreport.write("        Monthly Usage Statistics           \n")
+    #
+    # nreport.write("                Statistics                 \n")
+    # nreport.write("Total Request Count  -  " + str(totalRequests) + '\n')
+    # nreport.write("3XX Status Codes  -  " +str(status300count) +' ('+ str(round(status300count/totalRequests*100,2)) +'%)\n')
+    # nreport.write("4XX Status Codes  -  " +str(status400count) +' ('+ str(round(status400count/totalRequests*100,2)) +'%)\n')
+    # nreport.write("Malformed Log Entries  -  " +str(malformedEntrys) +' ('+ str(round(malformedEntrys/totalRequests*100,2)) +'%)\n')
+    # nreport.write("Most Requested File  -  " +str(max(accessedFiles, key=accessedFiles.get) +' ('+ str(accessedFiles.get(max(accessedFiles, key=accessedFiles.get), "none")) +' requests)\n'))
+    # nreport.write("Least Requested File  -  " +str(min(accessedFiles, key=accessedFiles.get) +' ('+ str(accessedFiles.get(min(accessedFiles, key=accessedFiles.get), "none")) +' request)\n'))
+    # # nreport.write("Status Codes:\n")
     # for key in sorted(codefreq):
     #     nreport.write("  HTTP Status Code "+str(key)+ ' occurred '+ str(codefreq[key])+" times\n")
     # nreport.write("===========================================\n")
